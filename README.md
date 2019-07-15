d2f39a7dccaa9d6f1828dc21b99e67c8075c1197
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-07-15 15:04:42 +0500

    Edited readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # enumerable_methods
+In this project the following methods are defined: 
+<br>
+my_each, my_each_with_index, my_select, my_all?, my_any?, my_none?, my_count, my_map, my_inject
+
+<br>
+Built with:
+* Ruby
+
+# Author
+* Abdusaid10 https://github.com/Abdusaid10
--- enums.rb
+++ enums.rb
@@ -1,101 +1,101 @@
 module Enumerable
-    def my_each
-        for item in self do
-            yield(item)
-        end
+  def my_each
+    for item in self do
+      yield(item)
     end
+  end
 
-    def my_each_with_index
-        i=0
-        for item in self do
-            yield(item, i)
-            i+=1
-        end
+  def my_each_with_index
+    i=0
+    for item in self do
+      yield(item, i)
+      i+=1
     end
+  end
 
-    def my_select
-      arr=[]
+  def my_select
+    arr=[]
       self.my_each do
-            |item| arr << item if yield(item)
-        end
-        arr
-    end
+        |item| arr << item if yield(item)
+      end
+    arr
+  end
 
-    def my_all?()
-        check=false
-        self.my_each do |item|
-            check = yield(item)
-            break if not check
-        end
-        check
-    end
-
-    def my_any?
-        check = false
-        self.my_each do |item|
-            check=yield(item)
-            break if check
-        end 
-        check
-    end
-
-    def my_none?()
-      check = false
+  def my_all?()
+    check=false
       self.my_each do |item|
-        check = !yield(item)
-        break if not check  
+        check = yield(item)
+        break if not check
+      end
+    check
+  end
+
+  def my_any?
+    check = false
+      self.my_each do |item|
+        check=yield(item)
+        break if check
       end 
       check
+  end
+
+  def my_none?()
+    check = false
+    self.my_each do |item|
+      check = !yield(item)
+      break if not check  
+    end 
+    check
+  end
+
+  def my_count(arg=nil)
+    arr=[]
+    if arg != nil
+      self.my_each do |i|
+        arr << i if arg == i
+        end
+    end
+    if block_given? == false
+      self.my_each do |i|
+      arr << i
+    end
+  end
+    self.my_each do |i|
+      arr << i if yield(i)
     end
 
-    def my_count(arg=nil)
-        arr=[]
-        if arg != nil
-            self.my_each do |i|
-                arr << i if arg == i
-            end
-        end
-        if block_given? == false
-            self.my_each do |i|
-                arr << i
-            end
-        end
-        self.my_each do |i|
-            arr << i if yield(i)
-        end
+    arr.length
+  end
 
-        arr.length
+  def my_map(&proc)
+    arr=[]
+    return self unless proc or block_given?
+    self.my_each do |item|   
+      if block_given?
+        arr << yield(item)
+      else
+        arr << proc.call(item)
+      end
     end
+    arr
+  end
 
-    def my_map(&proc)
-        arr=[]
-        return self unless proc or block_given?
-        self.my_each do |item|   
-            if block_given?
-                arr << yield(item)
-            else
-                arr << proc.call(item)
-            end
-        end
-        arr
+  def my_inject(*arg)
+    ac=self[0]
+    for item in self[1..self.length] do
+      ac=yield(ac,item)
     end
-
-    def my_inject(*arg)
-        ac=self[0]
-        for item in self[1..self.length] do
-            ac=yield(ac,item)
-        end
-        ac
-    end
+    ac
+  end
 end
 
-####### CHECK FOR my_select
+####### TEST my_select
 # arr=[1,2,3,4,4]
 # # arr.my_select do |item|
 # #   puts  item.even?
 # # end
 
-# ###### CHECK FOR my_all?
+# ###### TEST my_all?
 # def all_ch(arr)
 #     arr.my_all? do |item|
 #         item.length >=2
@@ -104,7 +104,7 @@ end
 # str=["wordd", "any", "honey", "med", ""]
 # p all_ch(str)
 
-###### CHECK FOR my_any?
+###### TEST my_any?
 # def all_ch(arr)
 #     arr.my_any? do |item|
 #         item.length >=3
@@ -113,7 +113,7 @@ end
 # str=["wordd", "any", "honey", "med",""]
 # p all_ch(str)
 
-###### CHECK FOR my_none?
+###### TEST my_none?
 # def all_ch(arr)
 #     arr.my_none? do |item|
 #         item.size == 2
@@ -124,16 +124,16 @@ end
 # p all_ch(str)
 # p all_ch(str2)
 
-######### CHECK FOR my_count
+######### TEST my_count
 # nums = [1,2,3,4,5,6,7,8,9,10]
 # p nums.my_count(&:even?)
 
-######### CHECK FOR my_map
+######### TEST my_map
 # nums = [1,2,3,4,5,6,7,8,9,10]
 # p nums.map(&:even?)
 # p nums.my_map{ |i| i * 2 }
 
-######### CHECK FOR my_inject
+######### TEST my_inject
 # def multiply_els(arr)
 #     arr.my_inject do |item,e|
 #       item * e
